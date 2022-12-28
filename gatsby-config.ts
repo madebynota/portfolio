6babb5879baa5eeae51734e5ce38a595330f5a98
Bomani Oseni McClendon <bomanimc@gmail.com>
2022-12-27 20:06:01 -0500

    Add a favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -19,10 +19,8 @@ const config: GatsbyConfig = {
         start_url: ".",
         background_color: "#ffffff",
         theme_color: "#000000",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
