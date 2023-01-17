99962b93d7e8805e7cbb7d1aa9e6399912616aa6
jbirori <jeffbirori@gmail.com>
2023-01-16 21:10:52 -0500

    Implement button component and types according to Figma specs

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ButtonProps } from "./ButtonTypes";
+import Text from "../Text/Text";
+import "./ButtonStyles.scss";
+
+export const Button: React.FC<ButtonProps> = ({
+  buttonLabel,
+  className = "",
+  type = "solid",
+  onClick,
+  ...restProps
+}) => {
+
+  return (
+    <div
+      aria-label={buttonLabel}
+      role="button"
+      className={`button ${type}`}
+      onClick={onClick}
+      {...restProps}
+    >
+      <Text type="h6" className="button-label">
+        {buttonLabel}
+      </Text>
+    </div>
+  );
+};
+
+export default Text;
