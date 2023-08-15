ef5fd83caef630a3db9ac6565f182f8125010ca2
Bomani Oseni McClendon <bomanimc@gmail.com>
2023-08-14 20:16:10 -0400

    Address splash background issue

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -7,19 +7,21 @@ import heroImg from "../../images/hero-img-placeholder.png";
 import "./HeroSection.scss";
 
 export const HeroSection = () => (
-    <section className="site-section root">
-      <div className="content-grid">
-        <div className="text-section">
-          <Text className="headline" type="h2">
-            Event Apps for Everyone
-          </Text>
-          <Text type="p+">
-            Dial Up is powering the next generation of event apps for organizers and attendees
-          </Text> 
-          <Button buttonLabel="request demo" onClick={() => alert("Email us at hello@dialup.digital")} />
-        </div>
-        <div className="image-section">
-          <img src={heroImg} />
+    <section className="root">
+      <div className="site-section">
+        <div className="content-grid">
+          <div className="text-section">
+            <Text className="headline" type="h2">
+              Event Apps for Everyone
+            </Text>
+            <Text type="p+">
+              Dial Up is powering the next generation of event apps for organizers and attendees
+            </Text> 
+            <Button buttonLabel="request demo" onClick={() => alert("Email us at hello@dialup.digital")} />
+          </div>
+          <div className="image-section">
+            <img src={heroImg} />
+          </div>
         </div>
       </div>
     </section>
