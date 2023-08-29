f5c9e805858c19a22fce7df0c51c794f4eb8b468
Bomani Oseni McClendon <bomanimc@gmail.com>
2023-08-29 15:25:42 -0400

    Move home page to component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { FeatureBlock } from "./FeatureBlock/FeatureBlock";
 export { Footer } from "./Footer/Footer";
 export { Header } from "./Header/Header";
 export { HeroSection } from "./HeroSection/HeroSection";
+export { HomePage } from "./HomePage/HomePage";
 export { ImageTextBlock } from "./ImageTextBlock/ImageTextBlock";
 export { Layout } from "./Layout/Layout";
 export { SEO } from "./SEO/SEO";
