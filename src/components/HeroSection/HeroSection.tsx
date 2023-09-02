6202e1c531efadd24a6659941599d098a69e9444
Jeffrey Birori <jeffbirori@gmail.com>
2023-09-01 23:48:32 -0400

    Update mobile Hero (#40)
    * Update Hero section to follow the updated mobile specs in Figma and include some placeholder images
    
    * Use spacing variable

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -5,27 +5,35 @@ import {
 } from "../../components";
 import heroImg from "../../images/hero-img-placeholder.png";
 import logo from "../../images/dialup-retro-logo.png";
+import heroBg from "../../images/noise_bg.png";
+import mobileImg from "../../images/mobile_hero.png";
 import "./HeroSection.scss";
 import { openDemoForm } from "../../utils/global";
 
 export const HeroSection = () => (
-    <section className="root">
-      <div className="site-section">
-        <div className="content-grid">
-          <img className="logo" src={logo} alt="Dial Up Logo" />
-          <div className="text-section">
-            <Text className="headline" type="h2">
-              Event Apps for Everyone
-            </Text>
-            <Text type="p+">
-              Dial Up is powering the next generation of event apps for organizers and attendees
-            </Text> 
-            <Button buttonLabel="request demo" onClick={openDemoForm} />
+  <section className="hero">
+    <div className="hero__bg">
+      <img src={heroBg} />
+    </div>
+    <div className="site-section">
+      <div className="content-grid">
+        <img className="logo" src={logo} alt="Dial Up Logo" />
+        <div className="text-section">
+          <Text className="headline" type="h2" mobileType="h4">
+            Event Apps for Everyone
+          </Text>
+          <div className="mobile-image-section">
+            <img src={mobileImg} />
           </div>
-          <div className="image-section">
-            <img src={heroImg} />
-          </div>
+          <Text className="subheadline" type="p+" mobileType="p">
+            Dial Up is powering the next generation of event apps for organizers and attendees
+          </Text> 
+          <Button buttonLabel="request demo" onClick={openDemoForm} />
+        </div>
+        <div className="image-section">
+          <img src={heroImg} />
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 );
