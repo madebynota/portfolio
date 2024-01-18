b7b465ce136f7c4b453e5dfe9273d1b2c88270db
Bomani Oseni McClendon <bomanimc@gmail.com>
2024-01-18 13:25:36 -0500

    Create a new AppsPageSection grid component

---
+++ src/components/AppsPageSection/AppsPageSectionTypes.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+
+export type AppsPageSectionProps = {
+  /**
+   * Custom class(es) to apply
+   */
+  className?: string;
+
+  /**
+   * Custom class(es) to apply
+   */
+   children?: React.ReactNode;
+};
